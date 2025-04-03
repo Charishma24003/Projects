015553fc650e53d8d4ee15cc9f75a43553a721a0
Charishma24003 <charishmais003@gmail.com>
2025-04-03 21:47:54 +0530

    Create seva.php

---
+++ Amrutheshwara/seva.php
@@ -0,0 +1,410 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- stella-orre/services-dark.html  30 Nov 2019 03:49:30 GMT -->
+
+<head>
+	<meta charset="utf-8">
+	<title>SHREE AMRUTHESHWARA TEMPLE| SEVA</title>
+	<!-- Stylesheets -->
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+	<link href="css/bootstrap.css" rel="stylesheet">
+
+	<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+	<link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+	<!-- Responsive -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]-->
+	<!--[if lt IE 9]><script src="js/respond.js"></script><![endif]-->
+</head>
+
+<body>
+
+	<div class="page-wrapper">
+		<!-- Preloader -->
+		<div class="preloader"></div>
+
+		<header class="main-header header-style-one">
+			<!-- Header Upper -->
+			<div class="header-upper">
+				<div class="inner-container">
+					<div class="header-main">
+						<!--Info-->
+						<a href="index.php" class="logo-link">
+							<img src="newlogo.webp" alt="" title="" width="auto" height="80px" />
+						</a>
+						<!--Nav Box-->
+						<div class="nav-outer clearfix">
+							<!--Mobile Navigation Toggler For Mobile-->
+							<div class="mobile-nav-toggler"><span class="icon flaticon-menu-1"></span></div>
+							<nav class="main-menu navbar-expand-md navbar-light">
+								<div class="navbar-header">
+									<!-- Togg le Button -->
+									<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+										<span class="icon flaticon-menu-1"></span>
+									</button>
+								</div>
+
+								<div class="collapse navbar-collapse clearfix" id="navbarSupportedContent">
+									<ul class="navigation clearfix">
+										<li><a href="index.php">HOME</a>
+										<li class=" dropdown"><a href="about.php">ABOUT</a>
+											<ul>
+												<li><a href="about.php">About the Temple</a></li>
+												<li><a href="history.php">History of the Temple</a></li>
+											</ul>
+										</li>
+										<li class="current dropdown"><a href="seva.php">SEVAS</a>
+											<ul>
+												<li><a href="seva.php">SEVAS</a></li>
+												<li><a href="sevatable.php">TABLE OF SEVAS</a></li>
+											</ul>
+										</li>
+										<li class=" dropdown"><a href="ptimings.php">POINTS TO BE NOTED</a>
+											<ul>
+												<li><a href="ptimings.php">Pooja Timings</a></li>
+												<li><a href="rules.php">Rules to be followed</a></li>
+												<li><a href="direction.php">Directions to reach the Temple</a></li>
+											</ul>
+										</li>
+										<li><a href="gallery.php">GALLERY</a>
+										</li>
+										<li><a href="donation.php">DONATION</a>
+										</li>
+										<li><a href="contact.php">CONTACT</a></li>
+										<li><a href="register.php">REGISTER</a></li>
+									</ul>
+								</div>
+							</nav>
+							<!-- Main Menu End-->
+
+							<!-- Outer Box -->
+							<div class="outer-box clearfix">
+								<div class="search-box-btn"><span class="icon flaticon-magnifying-glass-1"></span></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--End Header Upper-->
+
+			<!-- Mobile Menu  -->
+			<div class="mobile-menu">
+				<div class="menu-backdrop"></div>
+				<div class="close-btn"><span class="icon flaticon-cancel"></span></div>
+
+				<nav class="menu-box">
+					<div class="nav-logo"><a href="index-2.html"><img src="images/logo.png" alt="" title=""></a></div>
+					<ul class="navigation clearfix"><!--Keep This Empty / Menu will come through Javascript--></ul>
+					<!--Social Links-->
+					<div class="social-links">
+						<ul class="clearfix">
+							<li><a href="#"><span class="fab fa-twitter"></span></a></li>
+							<li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
+							<li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
+							<li><a href="#"><span class="fab fa-instagram"></span></a></li>
+							<li><a href="#"><span class="fab fa-youtube"></span></a></li>
+						</ul>
+					</div>
+				</nav>
+			</div><!-- End Mobile Menu -->
+
+		</header>
+		<!-- End Main Header -->
+
+		<!--Page Title-->
+		<section class="page-title" style="background-image:url(tempimgs/img19.jpg)">
+			<div class="auto-container">
+				<h2>SEVAS</h2>
+				<ul class="page-breadcrumb">
+					<li><a href="index-2.html">HOME</a></li>
+					<li>SEVAS OFFERED</li>
+				</ul>
+			</div>
+		</section>
+		<!--End Page Title-->
+
+		<!-- Services Section Four -->
+		<section class="services-section-four">
+			<div class="auto-container">
+
+				<!-- Sec Title -->
+				<div class="sec-title light centered">
+					<h1 style="color:#fff">SEVAS OFFERED</h1>
+					<p class="text" style="text-indent:50px">We offer a range of simple yet heartfelt services to the Lord.Through these offerings,we create a space for devotees to connect with the divine presence of Lord Shiva,fostering a sense of spiritual fulfillment and inner peace...</p>
+				</div>
+
+				<div class="sevagrid">
+					<!-- Service Block -->
+					<div class="sevacard">
+						<div class="inner-box wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+							<div class="image">
+								<img src="tempimgs/pic15.jpg" alt="" width="100%" height="250px" />
+							</div>
+							<div class="lower-content">
+								<h3>RUDRABHISHEKA</h3>
+								<div class="text">In Rudrabhisheka, sacred Kalashas are filled with water and the presence of Varuna(The God of the Celestial Ocean) and holy rivers namely Ganga, Yamuna, Godavari, Saraswati, Narmada, Sindhu and Kaveri are invoked into the water. With that water alone the Lord will be anointed by chanting the hymns, especially the Rudra Chamaka. </div>
+
+							</div>
+						</div>
+					</div>
+
+					<!-- Service Block -->
+					<div class="sevacard">
+						<div class="inner-box wow fadeInUp" data-wow-delay="250ms" data-wow-duration="1500ms">
+							<div class="image">
+								<img src="tempimgs/shivaidol.jpg" alt="" width="100%" height="250px" />
+							</div>
+							<div class="lower-content">
+								<h3>SHIVA SAHASRANAMA<br> ARCHANA</h3>
+								<div class="text">The Shiva Sahasranama is a Sanskrit hymn<br> that contains a list of the 1,000 names of Shiva, one of the principal deities of Hinduism and the supreme being in Shaivism. In Hindu tradition, a sahasranama is a type of devotional hymn listing the thousand names of a deity. </div>
+
+							</div>
+						</div>
+					</div>
+
+					<!-- Service Block -->
+					<div class="sevacard">
+						<div class="inner-box wow fadeInUp" data-wow-delay="500ms" data-wow-duration="1500ms">
+							<div class="image">
+								<img src="tempimgs/pic16.jpg" alt="" width="100%" height="250px" />
+							</div>
+							<div class="lower-content">
+								<h3>LINGARCHANA</h3>
+								<div class="text">Maha Lingarchana Puja involves performing of Pooja individually to 365 deities arranged as per the abode of Lord Shiva. This is also called “Kailasa Yantra Prastaram”. Abhishekam is carried out with 11 types of items to 365 deities.Benefit of worshipping Lord Shiva in the form Maha Lingarchana will remove all the obstacles.</div>
+
+							</div>
+						</div>
+					</div>
+
+					<!-- Service Block -->
+					<div class="sevacard">
+						<div class="inner-box wow fadeInUp" data-wow-delay="750ms" data-wow-duration="1500ms">
+							<div class="image">
+								<img src="tempimgs/pic18.jpg" alt="" width="100%" height="250px" />
+							</div>
+							<div class="lower-content">
+								<h3>MAHAMRITYUNJAYA MANTRA JAPA</h3>
+								<div class="text">The Mahamrityunjaya Mantra Japa is said to rejuvenate, bestow health, wealth, a long life, peace, prosperity and contentment. The Mantra is a centuries old technique of connecting one to pure consciousness and bliss. Known as the Moksha Mantra of Lord Shiva, Maha Mrityunjay evokes the Shiva within and removes the fear of death.</div>
+
+							</div>
+						</div>
+					</div>
+
+					<!-- Service Block -->
+					<div class="sevacard">
+						<div class="inner-box wow fadeInUp" data-wow-delay="750ms" data-wow-duration="1500ms">
+							<div class="image">
+								<img src="tempimgs/pic19.jpg" alt="" width="100%" height="250px" />
+							</div>
+							<div class="lower-content">
+								<h3>SHIVA AARTHI</h3>
+								<div class="text">By doing aarti of Mahadev,all yiur bad deeds get resolved.Worshipping shiva on mondays is fruitful. Because Monday is dedicated to Lord Shiva.To get the blessings of Mahadev,devotees also observe fast on mondays and remain engrossed in Lord Shiva. By performing the aarti on this day,all the troubles of the devotees go away. Its good to do Shiva Aarti every Monday.</div>
+
+							</div>
+						</div>
+					</div>
+
+					<!-- Service Block -->
+					<div class="sevacard">
+						<div class="inner-box wow fadeInUp" data-wow-delay="750ms" data-wow-duration="1500ms">
+							<div class="image">
+								<img src="tempimgs/pic20.webp" alt="" width="100%" height="250px" />
+							</div>
+							<div class="lower-content">
+								<h3>BILVA PATRA ARCHANA</h3>
+								<div class="text">The most auspicious way of worshipping Lord Shiva is by Archana of Bilva leaves. This is a sacred tree and its leaves are very dear to Lord Shiva. A confluence of three leaves of Bilva is called Bilva Dal which is said to be a representation of Lord Shiva's three eyes. It is believed that all the places of pilgrimage dwell at the base of the Bilva Tree. These leaves are used in all the poojas.</div>
+
+							</div>
+						</div>
+					</div>
+
+					<!-- Service Block -->
+					<div class="sevacard">
+						<div class="inner-box wow fadeInUp" data-wow-delay="750ms" data-wow-duration="1500ms">
+							<div class="image">
+								<img src="tempimgs/pic21.jpg" alt="" width="100%" height="250px" />
+							</div>
+							<div class="lower-content">
+								<h3>PRADOSHA POOJA</h3>
+								<div class="text">The word “pradosham” itself means the removal of any kind of subtle or energetic impurity. India’s ancient rishis have discovered that these impurities, which are formed due to our actions and attitudes in previous births, are the cause of our suffering and turmoil. By performing certain pujas on Trayodasi, these impurities can be washed away.</div>
+
+							</div>
+						</div>
+					</div>
+
+					<!-- Service Block -->
+					<div class="sevacard">
+						<div class="inner-box wow fadeInUp" data-wow-delay="750ms" data-wow-duration="1500ms">
+							<div class="image">
+								<img src="tempimgs/pic22.jpg" alt="" width="100%" height="250px" />
+							</div>
+							<div class="lower-content">
+								<h3>MAHA SHIVARATHRI POOJA</h3>
+								<div class="text">Maha Shivratri day is considered as the day which gives us the gift to reach heaven directly. The person who follows all Puja vidhi on every Maha Shivratri is close to god. God will be always be closer to them in all situations. Maha Shivratri considered being one of the auspicious day, people perform a set of rituals for lord Shiva.</div>
+
+							</div>
+						</div>
+					</div>
+
+				</div>
+		</section>
+		<!-- End Services Section -->
+
+		<!--Main Footer-->
+		<footer class="main-footer">
+			<div class="auto-container">
+				<!--Widgets Section-->
+				<div class="widgets-section">
+					<div class="row clearfix">
+
+						<!--big column-->
+						<div class="big-column col-lg-6 col-md-12 col-sm-12">
+							<div class="row clearfix">
+
+								<!--Footer Column-->
+								<div class="footer-column col-lg-7 col-md-6 col-sm-12">
+									<div class="footer-widget logo-widget">
+										<div class="logo">
+											<a href="index-2.html"><h3 style="color:#fff">&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp SHREE <br>AMRUTHESHWARA</h3></a>
+										</div>
+										<div class="text" style="margin-left:10px">Shree Amrutheshwara Temple<br>Thiruvail, Vamanjoor</div>
+										<ul class="social-icons" style="margin-left:10px">
+											<li><a href="#"><span class="fab fa-facebook-f"></span></a></li>
+											<li><a href="#"><span class="fab fa-linkedin-in"></span></a></li>
+											<li><a href="#"><span class="fab fa-twitter"></span></a></li>
+											<li><a href="#"><span class="fab fa-google-plus-g"></span></a></li>
+										</ul>
+									</div>
+								</div>
+
+								<!--Footer Column-->
+								<div class="footer-column col-lg-5 col-md-6 col-sm-12">
+									<div class="footer-widget links-widget">
+										<h2>Quick links</h2>
+										<div class="widget-content">
+											<ul class="list">
+												<li><a href="about.php">ABOUT</a></li>
+												<li><a href="donation.php">DONATION</a></li>
+												<li><a href="#">TERMS & CONDITIONS</a></li>
+												<li><a href="#">FAQ</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+
+						<!--big column-->
+						<div class="big-column col-lg-6 col-md-12 col-sm-12">
+							<div class="row clearfix">
+
+								<!--Footer Column-->
+								<div class="footer-column col-lg-5 col-md-6 col-sm-12">
+									<div class="footer-widget contact-widget">
+										<h2>Contact Info</h2>
+										<div class="widget-content">
+											<a href="tel:1800-574-9687" class="contact-number"> 0824 226 2169</a>
+											<ul>
+												<li> ವಾಮಂಜೂರು ಶ್ರೀ ಅಮೃತೇಶ್ವರ ದೇವಸ್ಥಾನ</li>
+												<li>Email :<a href="shreeamrutheshwara@gmail"> shreeamrutheshwara@gmail</a></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+
+								<!--Footer Column-->
+								<div class="footer-column col-lg-7 col-md-6 col-sm-12">
+									<div class="footer-widget newsletter-widget">
+										<h2>Newsletter</h2>
+										<div class="text">Get Special offers & Discounts</div>
+										<!-- Newsletter Form -->
+										<div class="newsletter-form">
+											<form method="post" action="templateshub.net">
+												<div class="form-group">
+													<input type="email" name="email" value="" placeholder="Enter your email address" required >
+													<button type="submit" class="theme-btn btn-style-one"><span class="txt">Subscribe</span></button>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+				<!--Footer Bottom-->
+				<div class="footer-bottom clearfix">
+					<div class="pull-left">
+						<div class="copyright"><a href="templateshub.net">Templates Hub</a></div>
+					</div>
+					<div class="pull-right">
+						<a href="templateshub.net">Templates Hub</a>
+					</div>
+				</div>
+
+			</div>
+		</footer>
+
+	</div>
+	<!--End pagewrapper-->
+
+
+	<!--Scroll to top-->
+	<!--Scroll to top-->
+	<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-up"></span></div>
+
+	<!--Search Popup-->
+	<div id="search-popup" class="search-popup">
+		<div class="close-search theme-btn"><span class="flaticon-cancel"></span></div>
+		<div class="popup-inner">
+			<div class="overlay-layer"></div>
+			<div class="search-form">
+				<form method="post" action="templateshub.net">
+					<div class="form-group">
+						<fieldset>
+							<input type="search" class="form-control" name="search-input" value="" placeholder="TYPE HERE..." required>
+							<input type="submit" value="Search Now!" class="theme-btn">
+						</fieldset>
+					</div>
+				</form>
+
+				<br>
+				<h3>Recent Search Keywords</h3>
+				<ul class="recent-searches">
+					<li><a href="sevatable.php">Rudrabhisheka</a></li>
+					<li><a href="direction.php">Directions</a></li>
+					<li><a href="rules.php">Rules</a></li>
+					<li><a href="history.php">History</a></li>
+					<li><a href="sevatable.php">Pradosha pooja</a></li>
+				</ul>
+
+			</div>
+
+		</div>
+	</div>
+
+	<!--Scroll to top-->
+	<script src="js/jquery.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.fancybox.js"></script>
+	<script src="js/owl.js"></script>
+	<script src="js/wow.js"></script>
+	<script src="js/validate.js"></script>
+	<script src="js/appear.js"></script>
+	<script src="js/scrollbar.js"></script>
+	<script src="js/script.js"></script>
+</body>
+
+<!-- stella-orre/index-4.html  30 Nov 2019 03:49:07 GMT -->
+
+</html>
